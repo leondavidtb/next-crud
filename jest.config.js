2f2ff6a067552303bb670f7db91f78de9696f2e8
Leonardo David <leondavidtb@gmail.com>
2024-04-04 09:19:21 -0300

    test: add ignore file in jest

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
 	collectCoverageFrom: ["components/*.{js,jsx}"],
 	coverageDirectory: "coverage",
 	testEnvironment: "jsdom",
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+	setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+	coveragePathIgnorePatterns: ["/components/index.js"],
 };
